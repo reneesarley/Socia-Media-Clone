d3ab3fa65fe9cff521e9f65c0d50880c9c7158d4
Renee Sarley <reneesarley@gmail.com>
2018-08-26 08:14:59 -0700

    add structure for Profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from './Nav';
+import Profile from './Profile'
 
 function App(){
   return (
     <div>
       <Nav/>
+      <Profile/>
     </div>
   );
 }
